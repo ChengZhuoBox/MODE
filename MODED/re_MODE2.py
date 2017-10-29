a33c492415f688d3163b1d89b51b2f80fa30e0bc
程卓 <chengzhuo@chengzhuodeMacBook-Pro.local>
2017-10-29 13:43:09 +0800

    add calculatorr

---
+++ MODED/re_MODE2.py
@@ -0,0 +1,7 @@
+
+#-*- coding:utf-8 _*-  
+""" 
+@author:chengzhuo 
+@file: re_MODE2.py 
+@time: 2017/10/25 
+""" 
---
+++ calculator.py
@@ -0,0 +1,7 @@
+
+#-*- coding:utf-8 _*-  
+""" 
+@author:chengzhuo 
+@file: calculator.py 
+@time: 2017/10/27 
+""" 
